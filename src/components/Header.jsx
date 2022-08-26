8dd24396dd64c2e0b41a872b085a7876a7bef054
Sakina Tinwala <tinwalasakina515253@gmail.com>
2022-08-26 12:36:12 +0530

    Initial Commit..

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Header = props => {
+  return (
+    <div>{props.title}</div>
+  )
+}
+
+Header.defaultProps = {
+    title: 'Indecision'
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired
+}
+
+export default Header
